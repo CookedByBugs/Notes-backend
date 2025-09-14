aa65de0ed5f1f5ed586f63882ea2f0e15187ae97
Hashir <muhammadhashir5889@gmail.com>
2025-09-14 22:59:39 +0500

    bugs fixed

--- Routes/Notes/notes.route.js
+++ Routes/Notes/notes.route.js
@@ -3,17 +3,18 @@ const Note = require("../../models/Notes/notes.model");
 const notesRouter = express.Router();
 const User = require("../../models/Auth/auth.model");
 notesRouter.post("/create", async (req, res) => {
-  const { title, allowedUsers, access, content } = req.body;
+  const { title, allowedUsers, access, content, createdBy } = req.body;
   const users = await User.find({ email: { $in: allowedUsers } });
   try {
-    const newNote = await Note.create({
+    const note = await Note.create({
       title,
       content,
+      createdBy,
+      // createdBy: req.user._id,
       allowedUsers: users.map((u) => u._id),
       access: access || "Private",
     });
-    // newNote.save();
-    res.status(200).json({ msg: "Note created successfully" });
+    res.status(200).json({ msg: "Note created successfully", note});
   } catch (error) {
     res.status(500).json({ msg: "Internal server error" });
   }
